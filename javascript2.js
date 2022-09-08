e2a64a03a3a10d00f4a763bb05dec2e00257bc9a
rodrigo laime <rodrigolaime141@gmail.com>
2022-09-08 17:15:05 -0300

    swhit

--- javascript2.js
+++ javascript2.js
@@ -1,4 +1,3 @@
-/*-------------------OPERADORES----------------------*/
 //operadores aritmeticos +, -, *, /, %, ().
 let a = 5 + (5 - 10) * 3;
 
@@ -33,3 +32,59 @@ console.log(9 === 9 && 9 === "9");
 console.log(9 === 9 && "9" === "9");
 
 //--------Estructuras de control---------------------------------------------
+
+/* --------------- */
+let hora = 12;
+if (hora >= 0 && hora <= 5) {
+  console.log("dejame dormir");
+} else if (hora >= 6 && hora <= 11) {
+  console.log("buenos dias");
+} else if (hora >= 12 && hora <= 18) {
+  console.log("buenas tardes");
+} else {
+  console.log("buenas noches");
+}
+
+if (hora > 0 && hora < 6) {
+  console.log("dejame dormir");
+} else if (hora > 5 && hora < 12) {
+  console.log("buenos dias");
+} else if (hora > 11 && hora < 19) {
+  console.log("buenas tardes");
+} else {
+  console.log("buenas noches");
+}
+
+/* operador ternario similar a la condicion if pero solo aplica a una linea de codigo*/
+let edad = 17;
+let eresMayor = edad >= 18 ? "Eres mayor de edad" : "Eres menor de edad";
+console.log(eresMayor);
+
+/* switch cuando tengamos diferentes valores para una sola variable */
+let dia = 2;
+switch (dia) {
+  case 0:
+    console.log("Domingo");
+    break;
+  case 1:
+    console.log("Lunes");
+    break;
+  case 3:
+    console.log("Maertes");
+    break;
+  case 4:
+    console.log("Miercoles");
+    break;
+  case 5:
+    console.log("Jueves");
+    break;
+  case 6:
+    console.log("Viernes");
+    break;
+  case 7:
+    console.log("Sabado");
+    break;
+  default:
+    console.log("El dia no existe");
+    break;
+}
